1748bed5e74a6dbd0ccda4e478cc9031f40d91f9
tyVespA <migliavacca.marco08@gmail.com>
2023-02-14 10:07:44 +0100

    Complete project

--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import s from "../styles/Travel.module.css";
+import LocationIcon from "../assets/LocationIcon.svg";
 
 function Travel({ item }) {
   return (
     <div className={s.travelContainer}>
-      <img src="" alt="" className={s.imageUrl} />
+      <img src={item.imageUrl} alt="" className={s.imageUrl} />
       <div className={s.travelInfo}>
         <div className={s.countrySection}>
-          <img src="" alt="" />
+          <img src={LocationIcon} alt="" />
           <p className={s.location}>{item.location}</p>
-          <a href="" className={s.googleMapsUrl}></a>
+          <a href={item.googleMapsUrl} className={s.googleMapsUrl}>
+            View on Google Maps
+          </a>
         </div>
         <h2 className={s.title}>{item.title}</h2>
         <p className={s.date}>
           <span className={s.startDate}>{item.startDate}</span>
+          <span> - </span>
           <span className={s.endDate}>{item.endDate}</span>
         </p>
         <p className={s.description}>{item.description}</p>
--- src/components/helpers/Data.jsx
+++ src/components/helpers/Data.jsx
@@ -10,13 +10,27 @@ export default [
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
   },
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Teatro+dell'opera+di+Sydney/@-33.8567844,151.213108,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    imageUrl:
+      "https://images.unsplash.com/photo-1595740229246-cfdda61917c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80",
+  },
+  {
+    title: "Geirangerfjord",
+    location: "Norway",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Geirangerfjord/@62.1048309,7.0050026,12z/data=!3m1!4b1!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1661345441183-d3d10b1f4e97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
   },
 ];
